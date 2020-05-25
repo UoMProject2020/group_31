4ced7e0ff19e7221413b596f848d7a4dce4ed528
AkshayA55 <64130453+AkshayA55@users.noreply.github.com>
2020-05-25 23:25:29 +1000

    Uploading searchAPI.py code.

--- searchAPI.py
+++ searchAPI.py
@@ -1,5 +1,6 @@
 import json
 import tweepy
+import couchdb
 
 keys = {}
 key_file = open("keys")
@@ -24,14 +25,23 @@ tweetCount = 0
 parsed_tweet = []
 parsed_users = []
 
-# searchQuery = "#auspol"  # this is what we're searching for
-searchQuery = "#accidents OR #roadaccident OR #carcrash OR #roadrage OR #speedkills OR #roadinjury OR #roadsafty OR #rashdriving"  # this is what we're searching for
-searchQueryList = ["#accidents" , "#roadaccident" , "#fatality" , "#carcrash" , "#roadrage" , "#speedkills" , "#roadinjury" , "#roadsafty" , "#rashdriving"]
-mainSearchQuery = "#accidents"
-tweetsPerQry = 100  # this is the max the API permits
-fName = 'tweets.txt' # We'll store the tweets in a text file.
+file = open("auspol_tweets_uni.txt","r")
+for line in file:
+	json_text = json.loads(line.strip())
+	parsed_tweet.append(json_text['id'])
+print(len(parsed_tweet))
+file.close()
 
-f= open(fName, 'w', encoding='utf8')
+searchQuery = "#auspol"  # this is what we're searching for
+searchQueryList = ["#auspol"]
+mainSearchQuery = "#auspol"
+tweetsPerQry = 100  # this is the max the API permits
+fName = 'auspol_tweets_uni.txt' # We'll store the tweets in a text file.
+
+couch = couchdb.Server('http://admin:admin@172.26.134.65:5984/')
+db = couch['twitter']  # existing
+
+f= open(fName, 'a')
 
 while(True):
 	auth = tweepy.OAuthHandler(keys[key_to_be_used]['consumer_key'], keys[key_to_be_used]['consumer_secret'])
@@ -45,38 +55,34 @@ while(True):
 
 	try:
 		for tweet in tweepy.Cursor(api.search, q=searchQuery, tweet_mode="extended",count=tweetsPerQry).items():
-			json.dump(tweet._json, f)
-			f.write("\n")
-			print(tweet._json['geo'],tweet._json['coordinates'],tweet._json['place'])
-			print("Found_tweet_in_first_loop")
-			screen_name = tweet._json['user']['screen_name']
-			parsed_tweet.append(tweet._json['id'])
+			if tweet._json['id'] not in parsed_tweet:
+				parsed_tweet.append(tweet._json['id'])
+				json.dump(tweet._json, f)
+				f.write("\n")
+				db.save(tweet._json)
+				screen_name = tweet._json['user']['screen_name']
 
-			for tweet_user in tweepy.Cursor(api.user_timeline, screen_name=screen_name,tweet_mode="extended",count=tweetsPerQry).items():
-				# print("in second loop")
-				if tweet_user._json['id'] not in parsed_tweet:
-					parsed_tweet.append(tweet_user._json['id'])
-					if any(x in str(tweet_user._json) for x in searchQueryList):
-						print("Found_tweet_in_second_loop")
-						json.dump(tweet_user._json, f)
-						f.write("\n")
+				for tweet_user in tweepy.Cursor(api.user_timeline, q=searchQuery, screen_name=screen_name,tweet_mode="extended",count=tweetsPerQry).items():
+					if tweet_user._json['id'] not in parsed_tweet:
+						parsed_tweet.append(tweet_user._json['id'])
+						if any(x in str(tweet_user._json) for x in searchQueryList):
+							db.save(tweet_user._json)
+							json.dump(tweet_user._json, f)
+							f.write("\n")
 
-			for user in tweepy.Cursor(api.followers, screen_name=screen_name,tweet_mode="extended",count=tweetsPerQry).items():
-				# print(user)
-				user_name = user._json['screen_name']
-				if user_name not in parsed_users:
-					parsed_users.append(user_name)
-					# print(user_name)
-					for tweet_user1 in tweepy.Cursor(api.user_timeline, screen_name=user_name,
-													tweet_mode="extended", count=tweetsPerQry).items():
-						if tweet_user1._json['id'] not in parsed_tweet:
-							parsed_tweet.append(tweet_user1._json['id'])
-							if any(x in str(tweet_user1._json) for x in searchQueryList):
-								print("Found_tweet_in_third_loop")
-								json.dump(tweet_user1._json, f)
-								f.write("\n")
+				for user in tweepy.Cursor(api.followers, screen_name=screen_name,tweet_mode="extended",count=tweetsPerQry).items():
+					user_name = user._json['screen_name']
+					if user_name not in parsed_users:
+						parsed_users.append(user_name)
+						for tweet_user1 in tweepy.Cursor(api.user_timeline, q=searchQuery, screen_name=user_name,
+														tweet_mode="extended", count=tweetsPerQry).items():
+							if tweet_user1._json['id'] not in parsed_tweet:
+								parsed_tweet.append(tweet_user1._json['id'])
+								if any(x in str(tweet_user1._json) for x in searchQueryList):
+									db.save(tweet_user1._json)
+									json.dump(tweet_user1._json, f)
+									f.write("\n")
 	except tweepy.TweepError as e:
-		# Just exit if any error
 		print("some error : " + str(e))
 
 	if (key_to_be_used+1) >= total_keys:
