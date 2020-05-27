c4c239ba783c318fe99529f93fc30206c8651ae6
AkshayA55 <64130453+AkshayA55@users.noreply.github.com>
2020-05-27 10:04:01 +1000

    Add files via upload

--- WebAppTest/tab_2.py
+++ WebAppTest/tab_2.py
@@ -1,24 +1,52 @@
-import dash_core_components as dcc
+#--------------------------------
+#Group No. 31
+#Team Members
+#Akshay Agarwal, 1141290,Melbourne
+#Avi Patel,1143213,Melbourne
+#Monit Patel,1135025,Melbourne
+#Prabha Choudhary,1098776,Melbourne
+#Shubham Parakh,1098807,Melbourne
+#--------------------------------
+import requests
+import plotly.graph_objs as go
 import dash_html_components as html
-import plotly.express as px
-
-gapminder = px.data.gapminder()
-
-dimensions = []
+import dash_core_components as dcc
+import DataExtractor as de
 
 
-external_stylesheets = [ 'https://codepen.io/chriddyp/pen/bWLwgP.css 5' ]
-colors = {
-    'background': '#111111',
-    'background2': '#FF0',
-    'text': '#7FDBFF'
-}
+cities = ["Adelaide","Brisbane","Melbourne","Perth","Sydney"]
+colors1 = ["red","orange","blue","green","yellow"]
+unique_user = de.unique_user_cities()
+voters = de.voters_data()
+
+twitter = go.Pie(labels = cities,values=unique_user)
+aurin = go.Pie(labels = cities, values = voters)
+
 
 tab_2_layout = html.Div([
-    html.H1("Gapminder data"),
-    dcc.Graph(id="graph", style={"width": "75%", "display": "inline-block"},
-              figure=px.scatter(gapminder, x="gdpPercap", y="lifeExp", animation_frame="year",
-                                animation_group="country",
-                                size="pop", color="continent", hover_name="country",
-                                log_x=True, size_max=55, range_x=[ 100, 100000 ], range_y=[ 25, 90 ])),
+    html.Div([
+        html.H3('Distribution of twitter users across cities',style={
+            'textAlign': 'center'}
+        ),
+        dcc.Graph(id='graph',
+                  figure={
+                      'data': [ twitter ],
+                  },
+                  style={'padding-right': '1px'}
+                  )
+    ], style={'width': '50%', 'display': 'inline-block'}),
+
+    html.Div([
+
+        html.H3('Eligible voters across cities', style={
+            'textAlign': 'center',
+        }),
+        dcc.Graph(id='graph1',
+                  figure={
+                      'data': [ aurin ]
+                  }
+                  ),
+    ], style={'width': '50%', 'display': 'inline-block'}),
+
+    html.Div(id='page-1-content')
 ])
